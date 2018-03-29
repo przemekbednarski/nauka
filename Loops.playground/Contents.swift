cddcc5bf23762243849b913111435cd470100bbb
przemekbednarski <przemekbednarski@icloud.com>
2018-03-29 14:27:34 +0200

    pętelki

---
+++ Loops.playground/Contents.swift
@@ -0,0 +1,56 @@
+//: A Cocoa based Playground to present user interface
+
+import Cocoa
+
+var myFirstInt: Int = 0
+
+for _ in 1...5 {
+    myFirstInt+=1
+    print(myFirstInt)
+}
+
+for case let index in 1...100 where index % 3 == 0 {
+    print(index)
+}
+
+/* mniej elegancki kod -> więcej wierszy + zagnieżdżenie warunku wewnątrz pętli
+ 
+for index in 1...100 {
+    if index % 3 == 0 {
+        print(index)
+    }
+}
+*/
+
+var i = 1
+while i < 6 {
+    myFirstInt+=1
+    print(myFirstInt)
+    i+=1
+}
+
+var shields = 5
+var blastersOverheating = false
+var blasterFireCount = 0
+
+while shields > 0 {
+    
+    if blastersOverheating {
+        print("Broń się przegrzała! Zainicjowano proces schładzania.")
+        sleep(5)
+        print("Broń jest gotowa do użycia.")
+        sleep(1)
+        blastersOverheating = false
+        blasterFireCount = 0
+    }
+
+    if blasterFireCount > 100 {
+        blastersOverheating = true
+        continue
+    }
+    //strzelamy!
+    print("Strzelamy!")
+    
+    blasterFireCount+=1
+}
+
